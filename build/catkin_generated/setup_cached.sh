21fbff082860fc209f0aa807d29e2e14f1e66932
zhuzhengming <1548467606@qq.com>
2025-02-14 17:02:36 +0100

    update

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/zhuzhengming/workspace/Object_throwing/Muilti-Object_Trowing/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/zhuzhengming/workspace/Object_throwing/Muilti-Object_Trowing/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/noetic/bin:/home/zhuzhengming/.opam/default/bin:/home/zhuzhengming/.cabal/bin:/home/zhuzhengming/.ghcup/bin:/home/zhuzhengming/anaconda3/condabin:/home/zhuzhengming/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/cuda-11.3/bin:/usr/local/cuda-11.3/bin'
+export PKG_CONFIG_PATH="/home/zhuzhengming/workspace/Object_throwing/Muilti-Object_Trowing/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/zhuzhengming/workspace/Object_throwing/Muilti-Object_Trowing/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/zhuzhengming/workspace/Object_throwing/Muilti-Object_Trowing/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH='/home/zhuzhengming/workspace/Object_throwing/Muilti-Object_Trowing/src:/home/zhuzhengming/workspace/Object_throwing/iiwa_ros/src:/home/zhuzhengming/workspace/ws_livox/src:/home/zhuzhengming/workspace/rrt_based_exploration/src:/opt/ros/noetic/share'
